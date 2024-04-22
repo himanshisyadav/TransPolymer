6ad70463d0761a00f0d4993d66e6cd3e45cffb13
Himanshi Yadav <hyadav@midway3-login3.rcc.local>
2024-04-21 19:37:57 -0500

    New OOD prep_data.py changes

--- prep_data.py
+++ prep_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np 
 from sklearn.model_selection import train_test_split
 import pdb
-from sklearn.preprocessing import QuantileTransformer
+from sklearn.preprocessing import QuantileTransformer, StandardScaler
 
 def only_smiles(smiles_file, output_file):
     df_smiles = pd.read_csv(smiles_file)
@@ -87,46 +87,12 @@ def exact_paper_seq(num_file, smiles_file, output_file):
     df_input_final['conductivity_log'] = df_input['conductivity_log']
     df_input_final.to_csv(output_file, index = False)
 
-def exact_paper_seq_ood(num_file, smiles_file, output_file):
-    df_num = pd.read_csv(num_file)
-    # df_num = df_num.round(2)
-    df_smiles = pd.read_csv(smiles_file)
-    df_smiles.fillna('NAN_SMILES', inplace=True)
-    df_input = pd.concat([df_num, df_smiles], axis = 1)
-
-    # unique_rows = df_input.drop_duplicates(subset=['solv_comb_sm','salt_sm'])
-
-    # df_input = unique_rows
-
-    # pdb.set_trace()
-
-    #Using Quantile Transformer for Normalisation
-    # num_cols = [ 'solv_ratio_1', 'solv_ratio_2', 'solv_ratio_3', 'solv_ratio_4', 'mol_wt_solv_1', 'mol_wt_solv_2', 'mol_wt_solv_3', 'mol_wt_solv_4', 'conc_salt','temperature' ]
-    # for col_name in num_cols:
-    #     num_df = df_input[col_name]
-    #     num_df.replace('', 0, inplace=True)
-    #     # col_values = num_df.values.reshape(-1, 1)
-    #     # numerical_transformer = QuantileTransformer(output_distribution='normal')
-    #     # col_values_norm = numerical_transformer.fit_transform(col_values)
-    #     # df_input[col_name] = col_values_norm
-    #     df_input[col_name] = df_input[col_name].round(2)
-
-    df_input = df_input.applymap(str)   
-
-    df_input['solv_1'] = df_input[['solv_comb_sm', 'solv_ratio_1', 'mol_wt_solv_1']].apply("$".join, axis=1)
-    df_input['salt'] = df_input[['salt_sm', 'conc_salt']].apply("$".join, axis=1)
-    df_input_final = pd.DataFrame()
-    df_input_final['input'] = df_input[['solv_1', 'salt', 'temperature']].apply("|".join, axis=1)
-    df_input_final['conductivity_log'] = df_input['conductivity_log']
-    df_input_final.to_csv(output_file, index = False)
-
 def exact_paper_seq_ood_using_all_solvs(num_file, smiles_file, output_file):
     df_num = pd.read_csv(num_file)
     df_smiles = pd.read_csv(smiles_file)
     df_smiles.fillna('NAN_SMILES', inplace=True)
     df_input = pd.concat([df_num, df_smiles], axis = 1)
 
-    # pdb.set_trace()
     #Using only unique solvents and salts, temperature is ignored
     unique_rows = df_input.drop_duplicates(subset=['solv_ratio_1', 'solv_ratio_2', 'solv_ratio_3', 'solv_ratio_4','mol_wt_solv_1', 'mol_wt_solv_2', 'mol_wt_solv_3', 'mol_wt_solv_4','mol_wt_salt', 'conc_salt', 'solv_comb_sm', 'salt_sm'])
     df_input = unique_rows
@@ -153,34 +119,6 @@ def exact_paper_seq_ood_using_all_solvs(num_file, smiles_file, output_file):
     df_input_final.to_csv(output_file, index = False)
 
 
-def exact_paper_seq_ood_using_all_solvs_drop_negative(num_file, smiles_file, output_file):
-    df_num = pd.read_csv(num_file)
-    df_smiles = pd.read_csv(smiles_file)
-    df_smiles.fillna('NAN_SMILES', inplace=True)
-    df_input = pd.concat([df_num, df_smiles], axis = 1)
-    df_input['conductivity_log'] = np.log10(np.exp(df_input['conductivity_log']))
-
-    df_input = df_input[df_input['conductivity_log'] >= 0]
-
-    df_input['solv_1_sm'] = df_input['solv_comb_sm']
-    df_input['solv_2_sm'] = "NAN_SMILES"
-    df_input['solv_3_sm'] = "NAN_SMILES"
-    df_input['solv_4_sm'] = "NAN_SMILES"
-
-    df_input = df_input.applymap(str)   
-    df_input['solv_1'] = df_input[['solv_1_sm', 'solv_ratio_1', 'mol_wt_solv_1']].apply("$".join, axis=1)
-    df_input['solv_2'] = df_input[['solv_2_sm', 'solv_ratio_2', 'mol_wt_solv_2']].apply("$".join, axis=1)
-    df_input['solv_3'] = df_input[['solv_3_sm', 'solv_ratio_3', 'mol_wt_solv_3']].apply("$".join, axis=1)
-    df_input['solv_4'] = df_input[['solv_4_sm', 'solv_ratio_4', 'mol_wt_solv_4']].apply("$".join, axis=1)
-    df_input['salt'] = df_input[['salt_sm', 'conc_salt']].apply("$".join, axis=1)
-    df_input_final = pd.DataFrame()
-    df_input_final['input'] = df_input[['solv_1', 'solv_2', 'solv_3', 'solv_4', 'salt']].apply("|".join, axis=1)
-
-    df_input_final['temperature'] = df_input['temperature']
-    df_input_final['conductivity_log'] = df_input['conductivity_log']
-    df_input_final.to_csv(output_file, index = False)
-
-
 def exact_paper_seq_temp_separate(num_file, smiles_file, output_file):
     df_num = pd.read_csv(num_file)
     df_smiles = pd.read_csv(smiles_file)
@@ -188,29 +126,15 @@ def exact_paper_seq_temp_separate(num_file, smiles_file, output_file):
     df_input = pd.concat([df_num, df_smiles], axis = 1)
     df_input['conductivity_log'] = np.log10(np.exp(df_input['conductivity_log']))
 
-    # #Using Quantile Transformer for Normalisation
-    # num_cols = [ 'solv_ratio_1', 'solv_ratio_2', 'solv_ratio_3', 'solv_ratio_4', 'mol_wt_solv_1', 'mol_wt_solv_2', 'mol_wt_solv_3', 'mol_wt_solv_4', 'conc_salt','temperature' ]
-    # for col_name in num_cols:
-    #     num_df = df_input[col_name]
-    #     num_df.replace('', 0, inplace=True)
-    #     col_values = num_df.values.reshape(-1, 1)
-    #     numerical_transformer = QuantileTransformer(output_distribution='normal')
-    #     col_values_norm = numerical_transformer.fit_transform(col_values)
-    #     df_input[col_name] = col_values_norm
-    #     df_input[col_name] = df_input[col_name].round(2)
-
-    # df_num_new = df_input[num_cols].copy()
-    # df_num_new.to_csv(num_vocab_file)
-
     df_input = df_input.applymap(str)   
     df_input['solv_1'] = df_input[['solv_1_sm', 'solv_ratio_1', 'mol_wt_solv_1']].apply("$".join, axis=1)
     df_input['solv_2'] = df_input[['solv_2_sm', 'solv_ratio_2', 'mol_wt_solv_2']].apply("$".join, axis=1)
     df_input['solv_3'] = df_input[['solv_3_sm', 'solv_ratio_3', 'mol_wt_solv_3']].apply("$".join, axis=1)
     df_input['solv_4'] = df_input[['solv_4_sm', 'solv_ratio_4', 'mol_wt_solv_4']].apply("$".join, axis=1)
     df_input['salt'] = df_input[['salt_sm', 'conc_salt']].apply("$".join, axis=1)
+
     df_input_final = pd.DataFrame()
-    df_input_final['input'] = df_input[['solv_1', 'solv_2', 'solv_3', 'solv_4', 'salt']].apply("|".join, axis=1)
-    
+    df_input_final['input'] = df_input[['solv_1', 'solv_2', 'solv_3', 'solv_4', 'salt']].apply("|".join, axis=1)  
     df_input_final['temperature'] = df_input['temperature']
     df_input_final['conductivity_log'] = df_input['conductivity_log']
 
@@ -249,10 +173,40 @@ def exact_paper_seq_ood_using_all_solvs_temp(num_file, smiles_file, output_file)
     df_input_final['conductivity_log'] = df_input['conductivity_log']
     df_input_final.to_csv(output_file, index = False)
 
+def ood_scaled_fusion(num_file, smiles_file, output_file):
+    df_num = pd.read_csv(num_file)
+    df_smiles = pd.read_csv(smiles_file)
+    df_smiles.fillna('NAN_SMILES', inplace=True)
+    df_input = pd.concat([df_num, df_smiles], axis = 1)
+    df_input['conductivity_log'] = np.log10(np.exp(df_input['conductivity_log']))
+
+    num_cols = ['solv_ratio_1', 'solv_ratio_2', 'solv_ratio_3', 'solv_ratio_4', 'conc_salt', 'temperature']
+    scaler = StandardScaler()
+    df_input[num_cols] = scaler.fit_transform(df_input[num_cols])
+
+    df_input['solv_1_sm'] = df_input['solv_comb_sm']
+    df_input['solv_2_sm'] = "NAN_SMILES"
+    df_input['solv_3_sm'] = "NAN_SMILES"
+    df_input['solv_4_sm'] = "NAN_SMILES"
+
+    df_input = df_input.applymap(str)   
+    df_input['solv_1'] = df_input[['solv_1_sm', 'solv_ratio_1']].apply("$".join, axis=1)
+    df_input['solv_2'] = df_input[['solv_2_sm', 'solv_ratio_2']].apply("$".join, axis=1)
+    df_input['solv_3'] = df_input[['solv_3_sm', 'solv_ratio_3']].apply("$".join, axis=1)
+    df_input['solv_4'] = df_input[['solv_4_sm', 'solv_ratio_4']].apply("$".join, axis=1)
+    df_input['salt'] = df_input[['salt_sm', 'conc_salt']].apply("$".join, axis=1)
+    
+    df_input_final = pd.DataFrame()
+    df_input_final['input'] = df_input[['solv_1', 'solv_2', 'solv_3', 'solv_4', 'salt']].apply("|".join, axis=1)
+    df_input_final['temperature'] = df_input['temperature']
+    df_input_final['conductivity_log'] = df_input['conductivity_log']
+
+    df_input_final.to_csv(output_file, index = False)
+
 
 def main():
-    # all_dollar()
-    exact_paper_seq_temp_separate("./data/freqI_train_multi_comp_add.csv", "./data/freqI_train_multi_comp.csv", "./data/freqI_train_exact_paper_seq_common_log_temp_sep.csv")
+    ood_scaled_fusion("./data/cond_ood_final_add.csv", "./data/cond_ood_final_comp.csv", "./data/cond_ood_exact_paper_seq_scaled_temp_sep_no_mol.csv")
+    # exact_paper_seq_temp_separate("./data/freqI_train_multi_comp_add.csv", "./data/freqI_train_multi_comp.csv", "./data/freqI_train_exact_paper_seq_common_log_temp_sep.csv")
     # exact_paper_seq("./data/random_train_multi_add.csv", "./data/random_train_multi_comp.csv", "./data/random_train_exact_paper_seq.csv")
     # exact_paper_seq("./data/random_test_multi_add.csv", "./data/random_test_multi_comp.csv", "./data/random_test_exact_paper_seq_common_log_temp_concat.csv")
     # exact_paper_seq_ood_using_all_solvs_drop_negative("./data/ood_add.csv", "./data/ood_comp.csv", "./data/ood_exact_paper_seq_all_solvs_common_log_no_negative_conductivity_temp_sep.csv")
